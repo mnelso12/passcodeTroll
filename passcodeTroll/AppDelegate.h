741abc1180ff5ac89b8ba6fa7ebd6bd6aafd9a88
Madelyn <mnelso12@nd.edu>
2016-03-08 20:44:34 -0500

    Initial Commit

---
+++ passcodeTroll/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  passcodeTroll
+//
+//  Created by MadelynNelson on 3/8/16.
+//  Copyright (c) 2016 Madelyn Nelson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
